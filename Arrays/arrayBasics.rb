9ff5d85429380859352d2214333ed3f721a0d3c1
Radhika Kulkarni <34987908+RadhikaKulkarni21@users.noreply.github.com>
2025-11-14 18:25:30 +0000

    arrays and hashes

---
+++ Arrays/arrayBasics.rb
@@ -0,0 +1,14 @@
+# array declaration
+#number array
+var_array = [1,2,3]
+puts var_array[2]
+
+#string array
+string_array = ["cow", "dog", "cat"]
+puts string_array[2]
+
+#multidimensional array
+multi_arr = [[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]
+multi_arr.each {|x| puts "#{x}\n"}
+
+#array
