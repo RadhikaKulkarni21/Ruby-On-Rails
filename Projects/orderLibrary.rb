1eeb2389d56c19e804dbe017ac21d217e4f06d09
Radhika Kulkarni <34987908+RadhikaKulkarni21@users.noreply.github.com>
2025-11-14 21:20:01 +0000

    method blocks (#5)

---
+++ Projects/orderLibrary.rb
@@ -0,0 +1,12 @@
+def alphabetize(arr, rev=false)
+  arr.sort!
+  if rev == true
+    arr.reverse!
+  else
+    return arr
+  end
+end
+
+numbers = [2, 5, 3, 16, 0, 322, -5]
+
+puts alphabetize(numbers, false)
